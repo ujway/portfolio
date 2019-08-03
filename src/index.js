ddc2110f598968c8ec330236f758e261ea884892
ujway <satoyu313@gmail.com>
2019-08-03 14:50:28 +0900

    Setup react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hello, React!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Hello, React!';
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
