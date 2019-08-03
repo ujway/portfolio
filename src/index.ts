89aea20530dd4512b8894384dc48526fed25771a
ujway <satoyu313@gmail.com>
2019-08-03 20:15:50 +0900

    Lotate box object

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,15 @@
-import * as THREE from 'three'
+import Canvas from './Canvas';
+import './scss/style'
 
-window.addEventListener('DOMContentLoaded', () => {
-  const renderer = new THREE.WebGLRenderer()
-  renderer.setSize(800, 600)
-  document.body.appendChild(renderer.domElement)
-
-  const scene = new THREE.Scene()
-
-  const camera = new THREE.PerspectiveCamera(45, 800 / 600, 1, 10000)
-  camera.position.set(0, 0, 1000)
-
-  const geometry = new THREE.BoxGeometry(250, 250, 250)
-  const material = new THREE.MeshPhongMaterial({color: 0xff0000})
-  const box      = new THREE.Mesh(geometry, material)
-  box.position.z = -5
-  scene.add(box)
-
-  const light = new THREE.DirectionalLight(0xffffff)
-  light.position.set(1, 1, 1)
-  scene.add(light)
-
-  const tick = (): void => {
-    requestAnimationFrame(tick)
-
-    box.rotation.x += 0.05
-    box.rotation.y += 0.05
-
-    renderer.render(scene, camera)
+export default class TopPage {
+  constructor() {
+    const canvas = new Canvas()
+    window.addEventListener('mousemove', e => {
+      canvas.mouseMoved(e.clientX, e.clientY)
+    })
   }
-  tick()
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  new TopPage()
 })
